a12b90af9465e7eca26ece5a0592f2af258e914b
Parker Thomas <pthomas1997@gmail.com>
2023-07-24 14:02:56 -0400

    formatting

--- scripts/main.js
+++ scripts/main.js
@@ -1,51 +1,51 @@
 const firstDiv = document.getElementById('1');
-    firstDiv.classList.add('active');
+firstDiv.classList.add('active');
 
-    const firstIcon = firstDiv.querySelector('i');
-    firstIcon.classList.add('fa-beat');
+const firstIcon = firstDiv.querySelector('i');
+firstIcon.classList.add('fa-beat');
 
+
+// Get all the div containers inside the timeline
+const divContainers = document.querySelectorAll('.timeline .container');
+
+// Variable to keep track of the currently active div
+let activeIndex = 0;
+
+function reset() {
+    activeIndex = 0;
+    updateActiveDiv();
+}
+
+// Function to update the active div and scroll to it
+function updateActiveDiv() {
+    divContainers.forEach((div, index) => {
+        if (index === activeIndex) {
+            div.classList.add('active');
+            let icon = div.querySelector("i");
+            icon.classList.add("fa-beat");
+        } else {
+            div.classList.remove('active');
+            let icon = div.querySelector("i")
+            icon.classList.remove("fa-beat");
+        }
+    });
     
-    // Get all the div containers inside the timeline
-    const divContainers = document.querySelectorAll('.timeline .container');
+    // Scroll to the active div
+    divContainers[activeIndex].scrollIntoView({ behavior: 'smooth' });
+}
 
-    // Variable to keep track of the currently active div
-    let activeIndex = 0;
-
-    function reset() {
-        activeIndex = 0;
+// Next button functionality
+document.getElementById('nextBtn').addEventListener('click', () => {
+    if (activeIndex < divContainers.length - 1) {
+        activeIndex++;
         updateActiveDiv();
     }
+});
 
-    // Function to update the active div and scroll to it
-    function updateActiveDiv() {
-        divContainers.forEach((div, index) => {
-            if (index === activeIndex) {
-                div.classList.add('active');
-                let icon = div.querySelector("i");
-                icon.classList.add("fa-beat");
-            } else {
-                div.classList.remove('active');
-                let icon = div.querySelector("i")
-                icon.classList.remove("fa-beat");
-            }
-        });
-
-        // Scroll to the active div
-        divContainers[activeIndex].scrollIntoView({ behavior: 'smooth' });
+// Previous button functionality
+document.getElementById('prevBtn').addEventListener('click', () => {
+    if (activeIndex > 0) {
+        activeIndex--;
+        updateActiveDiv();
     }
-
-    // Next button functionality
-    document.getElementById('nextBtn').addEventListener('click', () => {
-        if (activeIndex < divContainers.length - 1) {
-            activeIndex++;
-            updateActiveDiv();
-        }
-    });
-
-    // Previous button functionality
-    document.getElementById('prevBtn').addEventListener('click', () => {
-        if (activeIndex > 0) {
-            activeIndex--;
-            updateActiveDiv();
-        }
-    });
+});
