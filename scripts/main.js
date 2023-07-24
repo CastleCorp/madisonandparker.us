f12c15fb22b94e70caf83581d95f22bce0d121c5
Parker Thomas <pthomas1997@gmail.com>
2023-07-24 14:51:20 -0400

    add pics

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
 const firstDiv = document.getElementById('1');
 firstDiv.classList.add('active');
 
@@ -35,7 +38,7 @@ function updateActiveDiv() {
 }
 
 // Next button functionality
-document.getElementById('nextBtn').addEventListener('click', () => {
+nextBtn.addEventListener('click', () => {
     if (activeIndex < divContainers.length - 1) {
         activeIndex++;
         updateActiveDiv();
@@ -43,9 +46,45 @@ document.getElementById('nextBtn').addEventListener('click', () => {
 });
 
 // Previous button functionality
-document.getElementById('prevBtn').addEventListener('click', () => {
+prevBtn.addEventListener('click', () => {
     if (activeIndex > 0) {
         activeIndex--;
         updateActiveDiv();
     }
 });
+
+const contentDivs = document.querySelectorAll('.content');
+const modal = document.getElementById('modal');
+const modalImage = document.getElementById('modalImage');
+const modalSubtitle = document.getElementById('modalSubtitle');
+const closeBtn = document.getElementById('closeBtn');
+const overlay = document.getElementById('overlay');
+
+const pictureFrames = document.querySelectorAll('.picture-frame');
+pictureFrames.forEach((content, index) => {
+    const image = content.querySelector('img');
+
+    pictureFrames[index].addEventListener('click', () => {
+    modalImage.src = image.src;
+    modalSubtitle.innerText = image.alt; // Set the alt text as the subtitle
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+    nextBtn.disabled = true;
+    prevBtn.disabled = true;
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    closeModal();
+});
+
+overlay.addEventListener('click', () => {
+    closeModal();
+});
+
+function closeModal() {
+    overlay.style.display = 'none';
+    modal.style.display = 'none';
+    nextBtn.disabled = false;
+    prevBtn.disabled = false;
+}
