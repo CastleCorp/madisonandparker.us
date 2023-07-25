f35b5f5b3b48c3cd6729f2d6fe4c98bb7abf4ae7
Parker Thomas <pthomas1997@gmail.com>
2023-07-25 11:19:57 -0400

    add instructions popup

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,9 @@ const modalSubtitle = document.getElementById('modalSubtitle');
 const closeBtn = document.getElementById('closeBtn');
 const overlay = document.getElementById('overlay');
 const pictureFrames = document.querySelectorAll('.picture-frame');
+const instructions = document.getElementById('instructions');
+const instructionsCheckbox = document.getElementById('instructionsCheckbox');
+const closeInstructions = document.getElementById('closeInstructions');
 
 // Set the first timeline container to be active at page load
 firstDiv.classList.add('active');
@@ -63,12 +66,12 @@ prevBtn.addEventListener('click', () => {
     }
 });
 /**
- * End active div, nav functionality
- */
+* End active div, nav functionality
+*/
 
 /**
- * Begin picture frame, overlay functionality
- */
+* Begin picture frame, overlay functionality
+*/
 // Add click listeners to each picture frame
 pictureFrames.forEach((content, index) => {
     let image = content.querySelector('img');
@@ -77,7 +80,7 @@ pictureFrames.forEach((content, index) => {
     pictureFrames[index].addEventListener('click', () => {
         modalImage.src = image.src;
         modalSubtitle.innerText = image.alt;
-
+        
         // Display modal with image, disable nav
         modal.style.display = 'block';
         overlay.style.display = 'block';
@@ -102,5 +105,49 @@ overlay.addEventListener('click', () => {
     closeModal();
 });
 /**
- * End picture frame, overlay functionality
- */
+* End picture frame, overlay functionality
+*/
+
+/**
+* Begin instructions modal functionality
+*/
+const getCookie = name => {
+    const nameExpression = `${name}=`;
+    const cookies = document.cookie.split(';');
+    const cookie = cookies.find(currentCookie => currentCookie.includes(nameExpression));
+    return cookie ? cookie.trim().substring(nameExpression.length, cookie.length) : null;
+}
+
+const setCookie = (name, value, expire = 365, path = '/') => {
+    const date = new Date();
+    date.setTime(date.getTime() + (expire * 24 * 3600 * 1000));
+    const expires = date.toUTCString();
+    document.cookie = `${name}=${value}; expires=${expires}; path=${path}`;
+}
+
+cookieName = 'showInstructions';
+const cookie = getCookie(cookieName);
+
+if (cookie == 'show') {
+    instructions.style.display = 'block';
+    overlay.style.display = 'block';
+} else {
+    instructions.style.display = 'none';
+    overlay.styl.display = 'none';
+}
+
+instructionsCheckbox.addEventListener('change', (event) => {
+    if (instructionsCheckbox.checked == true) {
+        setCookie(cookieName, 'show', 7);
+    } else {
+        setCookie(cookieName, 'dontShow', 7);
+    }
+});
+
+closeInstructions.addEventListener("click", (event) => {
+    instructions.style.display = 'none';
+    overlay.style.display = 'none';
+});
+/**
+* End instructions modal functionality
+*/
